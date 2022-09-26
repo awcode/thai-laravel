7a59725e85f4b399876dba64cac8d71e6b97195b
mnwalker <mark@awcode.com>
2022-09-26 12:17:54 +0700

    Thai numerals

--- src/Helpers/ThaiNumerals.php
+++ src/Helpers/ThaiNumerals.php
@@ -4,35 +4,24 @@ namespace Awcode\ThaiLaravel\Helpers;
 
 class ThaiNumerals
 {
-    
-    protected $value;
-    protected $arabic = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-    protected $thai = [ '๐', '๑', '๒', '๓', '๔', '๕', '๖', '๗', '๘', '๙'];
-    
-    public function __toString()
-    {
-        try 
-        {
-            return (string) $this->value;
-        } 
-        catch (Exception $exception) 
-        {
-            return '';
-        }
+    static function thaiArray(){
+        return $thai = [ '๐', '๑', '๒', '๓', '๔', '๕', '๖', '๗', '๘', '๙'];
+    }
+    static function arabicArray(){
+        return $arabic = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
     }
     
     static function toThai($value){
         
-        $this->value = str_replace($this->arabic, $this->thai, $value);
+        return str_replace(ThaiNumerals::arabicArray() , ThaiNumerals::thaiArray() , $value);
 
-        return $this->value;
     }
 
     static function toArabic($value){
         
-        $this->value = str_replace($this->thai, $this->arabic, $value);
+        return str_replace(ThaiNumerals::thaiArray(), ThaiNumerals::arabicArray(), $value);
 
-        return $this->value;
     }
     
+
 }
